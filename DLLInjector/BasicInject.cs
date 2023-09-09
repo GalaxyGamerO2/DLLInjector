885e37cbfc75abc08ca25f23b621d574b03af9cc
GalaxyGamerO2 <codecraftingmc+1@gmail.com>
2023-09-09 20:13:51 +0200

    Fixed Injection Bug.

--- DLLInjector/BasicInject.cs
+++ DLLInjector/BasicInject.cs
@@ -9,10 +9,10 @@ namespace DLLInjector
         [DllImport("kernel32.dll")]
         public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
         public static extern IntPtr GetModuleHandle(string lpModuleName);
 
-        [DllImport("kernel32", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
         static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
